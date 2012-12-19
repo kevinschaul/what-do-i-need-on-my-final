7b72081c2da6d9ef484ece6137dbdf26ef0477eb
Kevin Schaul <kevin.schaul@gmail.com>
2012-12-19 16:06:41 -0600

    Added basic input validation

--- js/script.js
+++ js/script.js
@@ -3,22 +3,44 @@ function handleSubmit() {
     var hopefull_grade = $("#hopefull-grade").val();
     var final_worth = $("#final-worth").val();
 
-    // Convert these from percentage to floats
-    current_grade = current_grade / 100.0;
-    hopefull_grade = hopefull_grade / 100.0;
-    final_worth = final_worth / 100.0;
+    if (
+        isValidNumber(current_grade)
+        && isValidNumber(hopefull_grade)
+        && isValidNumber(final_worth)
+    ) {
+         // Convert these from percentage to floats
+        current_grade = current_grade / 100.0;
+        hopefull_grade = hopefull_grade / 100.0;
+        final_worth = final_worth / 100.0;
 
-    var answer = (hopefull_grade - (current_grade * (1 - final_worth))) / (1 * final_worth);
-    console.log(answer);
+        var answer = (hopefull_grade - (current_grade * (1 - final_worth))) / (1 * final_worth);
 
-    html = "";
-    html += "<p>";
-    html += "You must get at least a ";
-    html +=  100 * answer.toFixed(2);
-    html += "% on your final.";
-    $("#result-target").html(html);
+        html = "";
+        html += "<p>";
+        html += "You must get at least a ";
+        html +=  100 * answer.toFixed(2);
+        html += "% on your final.";
+        $("#result-target").html(html);
+
+    } else {
+        // Numbers were not valid
+        html = "";
+        html += "<p>";
+        html += "Whoops, something isn't right. Are your inputs numbers?";
+        $("#result-target").html(html);
+    }
 
     return false; // Prevent default submit button behavior
+}
+
+function isValidNumber(n) {
+    // TODO upper limit?
+    if (!isNaN(n) && n > 0) {
+        return true;
+    }
+    else {
+        return false
+    }
 }
 
 $(document).ready(function() {
